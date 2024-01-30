d65ad95d465392ff637562f59774c9c2aa591892
Nataliia <natashazubleva4@gmail.com>
2024-01-30 19:10:50 +0000

    add vite-project

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+
+
+document.querySelector('#app').innerHTML = /*html*/ `
+<h1 className="text-3xl font-bold underline">
+Hello world!
+</h1>
+`
+
+
